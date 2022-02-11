f3744edc279bce873485a05241e12b38c5f70516
Rodelmar Ocampo Luna <joserodelmar@gmail.com>
2022-02-10 19:59:09 -0600

    Obtencion de datos

--- R/02-recount3.R
+++ R/02-recount3.R
@@ -2,3 +2,14 @@
 library("recount3")
 
 human_projects <- available_projects()
+
+SRP169065
+
+proj_info <- subset(
+  human_projects,
+  project == "SRP169065" & project_type == "data_sources"
+)
+
+rse_gene_SRP169065 <- create_rse(proj_info)
+
+assay(rse_gene_SRP169065, "counts") <- compute_read_counts(rse_gene_SRP169065)
