36f3d472adfe315bd53f344cd0544e6460b4792d
Rodelmar Ocampo Luna <joserodelmar@gmail.com>
2022-02-12 17:23:33 -0600

    Avance iSEE

--- R/02-recount3.R
+++ R/02-recount3.R
@@ -3,13 +3,22 @@ library("recount3")
 
 human_projects <- available_projects()
 
-SRP169065
+SRP133965
 
-proj_info <- subset(
-  human_projects,
-  project == "SRP169065" & project_type == "data_sources"
-)
+proj_info_interactive <- interactiveDisplayBase::display(human_projects)
 
-rse_gene_SRP169065 <- create_rse(proj_info)
+## Verifying only one line was sent
+stopifnot(nrow(proj_info_interactive) == 1)
+## Creating RSE object
+rse_gene_interactive <- create_rse(proj_info_interactive)
 
-assay(rse_gene_SRP169065, "counts") <- compute_read_counts(rse_gene_SRP169065)
+assay(rse_gene_interactive, "counts") <- compute_read_counts(rse_gene_interactive)
+
+rse_gene_SRP133965 <- expand_sra_attributes(rse_gene_interactive)
+colData(rse_gene_interactive)[
+  ,
+  grepl("^sra_attribute", colnames(colData(rse_gene_interactive)))
+]
+
+library("iSEE")
+iSEE::iSEE(rse_gene_SRP133965)
